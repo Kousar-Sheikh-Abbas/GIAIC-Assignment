4d93b68e083ccd0aff2e7a09e86b79f6037f6003
Kousar-Sheikh-Abbas <sheikhkousar1441@gmail.com>
2024-11-08 00:35:50 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ function page() {
   return (
     <div className="h-[600px] flex ml-10" style={{ backgroundImage: "url('/images/img1.png')" }}>
       <div className="w-1/2">
-        <h1 className="text-[80px] capitalize">Discover the world's <span className="text-slate-700"> hidden</span> wonders</h1>
+        <h1 className="text-[80px] capitalize">Discover the world&apos;s <span className="text-slate-700"> hidden</span> wonders</h1>
         <p className="text-[22px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui esse mollitia autem alias quibusdam quae pariatur distinctio nobis libero deleniti, dignissimos ea aliquid facere vitae voluptate architecto explicabo incidunt officiis.</p>
         <button className="mx-5 my-6 rounded-full bg-transparent border-solid border-2 border-black px-20 text-[22px]">Discover Now</button>
       </div>
